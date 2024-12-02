b2c726896b8e0737089d4b0114638f1cc2d8b5c0
tyywells <wellswt7165@g.ucla.edu>
2024-12-02 15:13:49 -0800

    exercise 3

--- server.js
+++ server.js
@@ -23,6 +23,23 @@ app.get('/roll/:dice', (req, res) => {
     }
 });
 
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
+app.get('/collectibles/:index', (req, res) => {
+    const index = req.params.index;
+    const item = collectibles[index];
+
+
+    if (item) {
+        res.send(`So, you want the ${item.name}? For $${item.price}, it can be yours!`); //I know it kinda looks weird to have two "$$" in a row, but i use one to actually be in the response because I think it looks better to have a "$" before the number which represents the prics
+      } else {
+        res.send("This item is not yet in stock. Check back soon!");
+      }
+    });
 
 
 app.listen(3000, () => {
